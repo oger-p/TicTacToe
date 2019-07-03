2791cc122dd8858fb30dd983ff2cf1b9c246a818
oger-p <pierreantoineoger@gmail.com>
2019-07-03 15:13:59 +0200

    Updates

---
+++ precache-manifest.45c46bdcac7a2064e92088655ce33cfe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e427d68018ed0496b1e73a7d3e027ec",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "d2d33f3e1fb897ff0b19",
+    "url": "/TicTacToe/static/css/main.542826df.chunk.css"
+  },
+  {
+    "revision": "265315049bdae28700b7",
+    "url": "/TicTacToe/static/js/2.d372d594.chunk.js"
+  },
+  {
+    "revision": "d2d33f3e1fb897ff0b19",
+    "url": "/TicTacToe/static/js/main.02efdd17.chunk.js"
+  },
+  {
+    "revision": "6670768c31502717d35c",
+    "url": "/TicTacToe/static/js/runtime~main.ae000fa4.js"
+  }
+]);
